63633313021a9e5257aea7d29cbe107244af7337
Gary Topping <toppinggary@hotmail.com>
2023-09-05 18:17:09 +0900

    scraping

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -4,9 +4,9 @@ class NewsArticlesController < ApplicationController
 
     # Scraping logic for each website
     if params[:search].present?
-      # @articles += scrape_bbc
+      @articles += scrape_bbc
       # @articles += scrape_cnn
-      @articles += scrape_reuters
+      # @articles += scrape_reuters
     end
     @articles
   end
@@ -21,12 +21,12 @@ class NewsArticlesController < ApplicationController
       @html_doc = Nokogiri::HTML.parse(@html_file)
       @html_doc.search('.ssrcss-rn9nnc-PromoSwitchLayoutAtBreakpoints.et5qctl0').each do |element|
         @doc = Nokogiri::HTML(element.inner_html)
-        @title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
-        @content = @doc.xpath('//div/div/p[1]').first.text
-        @link = @doc.css('a').first.attr('href')
-        @image = @doc.css('img').first.attr('src')
-        @published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
-        @bbc_articles << NewsArticle.create!(source: 'BBC', title: @title, content: @content, image: @image, link: @link, published: @published)
+        @bbc_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
+        @bbc_content = @doc.xpath('//div/div/p[1]').first.text
+        @bbc_link = @doc.css('a').first.attr('href')
+        @bbc_image = @doc.css('img').first.attr('src')
+        @bbc_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
+        @bbc_articles << NewsArticle.create!(source: 'BBC', title: @bbc_title, content: @bbc_content, image: @bbc_image, link: @bbc_link, published: @bbc_published)
       end
     end
     @bbc_articles
@@ -40,11 +40,11 @@ class NewsArticlesController < ApplicationController
       @html_doc = Nokogiri::HTML.parse(@html_file)
       @selected_div = @html_doc.css('div.container__text.container_list-images-with-description__text').each do |element|
         @doc = Nokogiri::HTML(element.inner_html)
-        @title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
-        @content = @doc.xpath('//div/div/p[1]').first.text
-        @link = @doc.css('a').first.attr('href')
-        @image = @doc.css('.image__picture').first.text
-        @published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
+        @cnn_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
+        @cnn_content = @doc.xpath('//div/div/p[1]').first.text
+        @cnn_link = @doc.css('a').first.attr('href')
+        @cnn_image = @doc.css('.image__picture').first.text
+        @cnn_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
         @cnn_articles << NewsArticle.create!(source: 'CNN')
       end
     end
@@ -59,13 +59,12 @@ class NewsArticlesController < ApplicationController
       @html_doc = Nokogiri::HTML.parse(@html_file)
       @html_doc.search('.media-story-card__legal__3kqtT').each do |element|
         @doc = Nokogiri::HTML(element.inner_html)
-        @title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
-        @content = @doc.xpath('//div/div/p[1]').first.text
-        @link = @doc.css('a').first.attr('href')
-        @image = @doc.css('img').first.attr('src')
-        @published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
-        raise
-        @reuters_articles << NewsArticle.create!(source: 'REUTERS', title: @title, content: @content, image: @image, link: @link, published: @published)
+        @reuters_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
+        @reuters_content = @doc.xpath('//div/div/p[1]').first.text
+        @reuters_link = @doc.css('a').first.attr('href')
+        @reuters_image = @doc.css('img').first.attr('src')
+        @reuters_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
+        @reuters_articles << NewsArticle.create!(source: 'REUTERS')
       end
     end
     @reuters_articles
