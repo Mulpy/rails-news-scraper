81cac3bbef631f2bde77668f3725299245d5b5fe
Gary Topping <toppinggary@hotmail.com>
2023-09-15 18:05:53 +0900

    nyt scraped

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -7,6 +7,7 @@ class NewsArticlesController < ApplicationController
       @articles += scrape_bbc
       # @articles += scrape_cnn
       # @articles += scrape_reuters
+      @articles += scrape_nyt
     end
     @articles
   end
@@ -32,41 +33,60 @@ class NewsArticlesController < ApplicationController
     @bbc_articles
   end
 
-  def scrape_cnn
-    if params[:search].present?
-      @cnn_articles = []
-      @url = "https://edition.cnn.com/search?q=#{params[:search][:query]}&from=0&size=10&page=1&sort=newest&types=all&section="
-      @html_file = URI.open(@url).read
-      @html_doc = Nokogiri::HTML.parse(@html_file)
-      @selected_div = @html_doc.css('div.container__text.container_list-images-with-description__text').each do |element|
-        @doc = Nokogiri::HTML(element.inner_html)
-        @cnn_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
-        @cnn_content = @doc.xpath('//div/div/p[1]').first.text
-        @cnn_link = @doc.css('a').first.attr('href')
-        @cnn_image = @doc.css('.image__picture').first.text
-        @cnn_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
-        @cnn_articles << NewsArticle.create!(source: 'CNN')
-      end
-    end
-    @cnn_articles
-  end
+  # def scrape_cnn
+  #   if params[:search].present?
+  #     @cnn_articles = []
+  #     @url = "https://edition.cnn.com/search?q=#{params[:search][:query]}&from=0&size=10&page=1&sort=newest&types=all&section="
+  #     @html_file = URI.open(@url).read
+  #     @html_doc = Nokogiri::HTML.parse(@html_file)
+  #     @selected_div = @html_doc.css('div.container__text.container_list-images-with-description__text').each do |element|
+  #       @doc = Nokogiri::HTML(element.inner_html)
+  #       @cnn_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
+  #       @cnn_content = @doc.xpath('//div/div/p[1]').first.text
+  #       @cnn_link = @doc.css('a').first.attr('href')
+  #       @cnn_image = @doc.css('.image__picture').first.text
+  #       @cnn_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
+  #       @cnn_articles << NewsArticle.create!(source: 'CNN')
+  #     end
+  #   end
+  #   @cnn_articles
+  # end
 
-  def scrape_reuters
+  # def scrape_reuters
+  #   if params[:search].present?
+  #     @reuters_articles = []
+  #     @url = "https://www.reuters.com/site-search/?query=#{params[:search][:query]}"
+  #     @html_file = URI.open(@url).read
+  #     @html_doc = Nokogiri::HTML.parse(@html_file)
+  #     @html_doc.search('div.media-story-card__legal__3kqtT').each do |element|
+  #       @doc = Nokogiri::HTML(element.inner_html)
+  #       @reuters_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
+  #       @reuters_content = @doc.xpath('//div/div/p[1]').first.text
+  #       @reuters_link = @doc.css('a').first.attr('href')
+  #       @reuters_image = @doc.css('img').first.attr('src')
+  #       @reuters_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
+  #       @reuters_articles << NewsArticle.create!(source: 'REUTERS')
+  #     end
+  #   end
+  #   @reuters_articles
+  # end
+
+  def scrape_nyt
     if params[:search].present?
-      @reuters_articles = []
-      @url = "https://www.reuters.com/site-search/?query=#{params[:search][:query]}"
+      @nyt_articles = []
+      @url = "https://www.nytimes.com/search?query=#{params[:search][:query]}"
       @html_file = URI.open(@url).read
       @html_doc = Nokogiri::HTML.parse(@html_file)
-      @html_doc.search('.media-story-card__legal__3kqtT').each do |element|
+      @html_doc.search('.css-1i8vfl5').each do |element|
         @doc = Nokogiri::HTML(element.inner_html)
-        @reuters_title = @doc.css('p.ssrcss-6arcww-PromoHeadline.exn3ah96 span').first.text
-        @reuters_content = @doc.xpath('//div/div/p[1]').first.text
-        @reuters_link = @doc.css('a').first.attr('href')
-        @reuters_image = @doc.css('img').first.attr('src')
-        @reuters_published = @doc.css('span.ssrcss-1if1g9v-MetadataText.e4wm5bw1').text
-        @reuters_articles << NewsArticle.create!(source: 'REUTERS')
+        @nyt_title = @doc.css('h4.css-2fgx4k').first.text
+        @nyt_content = @doc.css('p.css-16nhkrn').first.text
+        @nyt_link = "https://www.nytimes.com#{@doc.css('a').first.attr('href')}"
+        @nyt_image = @doc.css('img').first.attr('src')
+        @nyt_published = @doc.css('span.css-bc0f0m').text
+        @nyt_articles << NewsArticle.create!(source: 'NYT', title: @nyt_title, content: @nyt_content, image: @nyt_image, link: @nyt_link, published: @nyt_published)
       end
     end
-    @reuters_articles
+    @nyt_articles
   end
 end
