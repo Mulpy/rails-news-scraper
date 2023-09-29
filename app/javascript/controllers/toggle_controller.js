44d8ad73fe82581a005fb81b007b181b6b461173
Gary Topping <toppinggary@hotmail.com>
2023-09-29 12:47:50 +0900

    checkboxes all working

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -5,11 +5,32 @@ export default class extends Controller {
   static targets = ["togglableBbc", "togglablePolitico", "togglableAl_jazeera", "togglableNyt"];
 
   connect() {
-    console.log("Hello from toggle_controller.js")
+    // console.log("Hello from toggle_controller.js")
   }
 
   bbc() {
     const divs = document.querySelectorAll('.bbc');
+    divs.forEach((div) => {
+      div.classList.toggle("d-none");
+    });
+  }
+
+  politico() {
+    const divs = document.querySelectorAll('.politico');
+    divs.forEach((div) => {
+      div.classList.toggle("d-none");
+    });
+  }
+
+  al_jazeera() {
+    const divs = document.querySelectorAll('.al_jazeera');
+    divs.forEach((div) => {
+      div.classList.toggle("d-none");
+    });
+  }
+
+  nyt() {
+    const divs = document.querySelectorAll('.nyt');
     divs.forEach((div) => {
       div.classList.toggle("d-none");
     });
