712298fb8d8b365f7108a971c6218ec66f0da26f
Gary Topping <toppinggary@hotmail.com>
2023-09-29 12:15:32 +0900

    can hide only one element - not all

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,7 +2,14 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ["togglableBbc", "togglablePolitico", "togglableAl_jazeera", "togglableNyt"];
+
   connect() {
     console.log("Hello from toggle_controller.js")
+  }
+
+  bbc() {
+    console.log("BBC firing");
+    this.togglableBbcTarget.classList.toggle("d-none");
   }
 }
