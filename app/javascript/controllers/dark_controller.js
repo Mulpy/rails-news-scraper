1c28c78eef26e62f7e3ebdfbbf434e1936038588
Gary Topping <toppinggary@hotmail.com>
2023-10-09 01:25:31 +0900

    toggling dark mode

--- app/javascript/controllers/dark_controller.js
+++ app/javascript/controllers/dark_controller.js
@@ -2,6 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="dark"
 export default class extends Controller {
+  static targets = ['dark']
+
   connect() {
     console.log('hello from dark_controller.js');
   }
@@ -9,17 +11,11 @@ export default class extends Controller {
   toggle() {
     document.body.classList.toggle('dark');
     console.log(this.darkTarget);
-    this.darkTarget.innerHTML =
-      `<button data-controller="dark"
-    data-action="click->dark#return"
-    data-dark-target='dark'>Toggle Light Mode</button>`;
+    this.darkTarget.innerHTML = `<button data-action="click->dark#return">Toggle Light Mode</button>`;
   }
 
   return() {
     document.body.classList.toggle('dark');
-    this.darkTarget.innerHTML =
-      `<button data-controller="dark"
-    data-action="click->dark#toggle"
-    data-dark-target='dark'>Toggle Dark Mode</button>`;
+    this.darkTarget.innerHTML = `<button data-action="click->dark#toggle">Toggle Dark Mode</button>`;
   }
 }
