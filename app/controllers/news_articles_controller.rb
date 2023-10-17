8574956652a181139d6682d07aa3042eccedd3f6
Gary Topping <toppinggary@hotmail.com>
2023-10-18 00:52:51 +0900

    log-in beautifying

--- app/controllers/news_articles_controller.rb
+++ app/controllers/news_articles_controller.rb
@@ -12,11 +12,11 @@ class NewsArticlesController < ApplicationController
     if params[:search].present?
       NewsArticle.destroy_all
       @articles += scrape_bbc
-      # @articles += scrape_politico
-      # @articles += scrape_al_jazeera
-      # @articles += scrape_nyt
-      # @articles += scrape_japan_times
-      # @articles += scrape_ap
+      @articles += scrape_politico
+      @articles += scrape_al_jazeera
+      @articles += scrape_nyt
+      @articles += scrape_japan_times
+      @articles += scrape_ap
 
       # Difficult to scrape websites --------------------------------------------------
       # @articles += scrape_cnn
@@ -24,7 +24,7 @@ class NewsArticlesController < ApplicationController
       # @articles += scrape_bloomberg
     else
       NewsArticle.destroy_all
-      # @articles += scrape_google
+      @articles += scrape_google
     end
     @articles
   end
