41180914bd20ab1d1bd34f222f977b07b4ec5b36
Gary Topping <toppinggary@hotmail.com>
2023-10-17 11:59:43 +0900

    devise user generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'news_articles/index'
   get 'news_scraper/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "news_articles#index"
-  resources :news_articles, only: %i[index show destroy]
+  resources :news_articles, only: %i[index show]
 end
