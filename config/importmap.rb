0dbc4b432e4de0c55d7d9d014d1b97f9c3ee795d
Gary Topping <toppinggary@hotmail.com>
2023-10-21 02:51:42 +0900

    star rating added

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin 'bootstrap', to: 'bootstrap.min.js', preload: true
 pin "@popperjs/core", to: "https://unpkg.com/@popperjs/core@2.11.2/dist/esm/index.js"
 pin "chart.js", to: "https://ga.jspm.io/npm:chart.js@4.4.0/dist/chart.js"
 pin "@kurkle/color", to: "https://ga.jspm.io/npm:@kurkle/color@0.3.2/dist/color.esm.js"
+pin "star-rating.js", to: "https://cdn.jsdelivr.net/npm/star-rating.js@4.3.0/dist/star-rating.esm.js"
